ed50add5ce3118220ae22dea0de2bed0d8e9ed12
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2020-11-03 20:49:33 +0000

    changed declaration for sentence from let to const

--- typewriter.js
+++ typewriter.js
@@ -1,4 +1,4 @@
-let sentence = "hello there from lighthouse labs";
+const sentence = "hello there from lighthouse labs";
 let counter = 0;
 for (const char of sentence) {
   setTimeout(() => process.stdout.write(char), counter);
